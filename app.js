b0f628ab5942b960b1e5d44cd5559dc0b69466e6
Dhanush <dk40828@gmail.com>
2023-01-08 00:34:06 +0530

    Updated

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+
+//Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 //connect DB
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication')
@@ -28,11 +34,17 @@ app.use(rateLimiter({
 }));
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 
 app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
